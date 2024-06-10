85cb3dfd7d3c9f4fd388d254e75507aa3d5c5ed7
Travis-DV <tfindley2@my.stlcc.edu>
2024-06-10 00:20:35 -0500

    Added the code

---
+++ InventoryMaintenance/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Windows;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace InventoryMaintenance
+{
+
+    public record Item
+    {
+        public int itemNumber;
+        public string description;
+        public double price;
+
+        public override string ToString()
+        {
+            return $"{itemNumber.ToString().PadLeft(7, '0')}; {description}, ${price}";
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Item> items = new List<Item>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            inventoryListbox.ItemsSource = items;
+
+            #region XML Loading
+            XElement TopLevelElement = XElement.Load("InventoryItems.xml");
+            /* THEY WILL NOT GO AWAY!!
+            List<string> TopLevelSplit = TopLevelElement.ToString().Split(System.Environment.NewLine).ToList();
+            for (int i = 0; i < TopLevelSplit.Count; i++)
+            {
+                if (TopLevelSplit[i] == "<Item />")
+                {
+                    TopLevelSplit.RemoveAt(i);
+                }
+                if (TopLevelSplit[i] == System.Environment.NewLine)
+                {
+                    TopLevelSplit.RemoveAt(i);
+                }
+            }
+            MessageBox.Show(string.Join(System.Environment.NewLine, TopLevelSplit));
+            */
+            
+            foreach (var item in TopLevelElement.Elements())
+            {
+                List<XElement> elements = item.Elements().ToList();
+                if (elements.Count == 0) //THEY WONT GO AWAY
+                {
+                    item.Remove();
+                    continue;
+                }
+                items.Add(new Item()
+                {
+                    itemNumber = (int)elements[0],
+                    description = (string)elements[1],
+                    price = (double)elements[2]
+                });
+                
+            }
+            #endregion
+        }
+
+        private void AddItem(Object sender, RoutedEventArgs e)
+        {
+            NewItem newItem = new NewItem(items.Select(ti => ti.itemNumber).ToList());
+            newItem.SelectedItem += AddToList;
+            newItem.Show();
+        }
+
+        private void AddToList(Item i)
+        {
+
+            items.Add(i);
+            inventoryListbox.Items.Refresh();
+            XDocument doc = XDocument.Load("InventoryItems.xml");
+            XElement element = doc.Element("Items");
+            element.Add(new XElement("Item",
+                new XElement("ItemNo", i.itemNumber.ToString()),
+                new XElement("Description", i.description),
+                new XElement("Price", i.price.ToString())));
+            doc.Save("InventoryItems.xml");
+        }
+
+        private void DeleteItem(Object sender, RoutedEventArgs e)
+        {
+            Item ItemToRemove = items[inventoryListbox.SelectedIndex];
+            items.Remove(ItemToRemove);
+            inventoryListbox.Items.Refresh();
+
+            
+
+            XDocument doc = XDocument.Load("InventoryItems.xml");
+            IEnumerable<XElement> itemElements = doc.Elements().Elements();
+            foreach (var item in itemElements)
+            {
+                IEnumerable<XElement> elements = item.Elements();
+                if (item.ToString() == "<Item />") //They will not go away i have no idea why but I only have 30 min left so bandaid
+                {
+                    elements.Remove();
+                    continue;
+                }
+                if (elements.ToList()[0].Value.ToString() == ItemToRemove.itemNumber.ToString())
+                {
+                    elements.Remove();
+                    doc.Save("InventoryITems.xml");
+                }
+
+            }
+        }
+
+        private void Exit(Object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ InventoryMaintenance/NewItem.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InventoryMaintenance
+{
+    /// <summary>
+    /// Interaction logic for NewItem.xaml
+    /// </summary>
+    public partial class NewItem : Window
+    {
+
+        public delegate void AddItem(Item i);
+        public event AddItem SelectedItem;
+        private List<int> illigals;
+
+        public NewItem(List<int> illigals)
+        {
+            InitializeComponent();
+            this.illigals = illigals;
+        }
+
+        private void NUMBERSONLY(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void DOUBLEONLY(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9,.]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void SaveItem(object sender, RoutedEventArgs e)
+        {
+            if (itemNoTB.Text == "" || descriptionTB.Text == "" || priceTB.Text == "")
+            {
+                return;
+            }
+            if (illigals.Contains(int.Parse(itemNoTB.Text)))
+            {
+                MessageBox.Show("That ID is already in use!");
+                return;
+            }
+
+            SelectedItem?.Invoke(new Item()
+            {
+                itemNumber = int.Parse(itemNoTB.Text),
+                description = descriptionTB.Text,
+                price = double.Parse(priceTB.Text)
+            });
+
+            this.Close();
+        }
+
+        private void CancelItem(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
